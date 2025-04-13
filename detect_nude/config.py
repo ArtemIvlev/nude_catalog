871483b1fcaa98d3e6c618b89d61c8be6ea95d38
Homoludens <artem.ivlev@gmail.com>
2025-04-13 21:19:21 +0300

    feat: добавлена поддержка переменных окружения в config.py

--- detect_nude/config.py
+++ detect_nude/config.py
@@ -1,20 +1,22 @@
-# Пути к файлам и директориям
 import os
+from dotenv import load_dotenv
 
-PHOTO_DIR = r"/mnt/smb/OneDrive/Pictures/!Фотосессии/"
-DB_FILE = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "DB", "database.db"))
-REVIEW_DIR = "review"
+# Загружаем переменные окружения
+load_dotenv()
 
-TELEGRAM_DB = os.path.abspath(os.path.join(os.path.dirname(__file__), "../..", "telegram_bot", "published_photos.sqlite"))
-
+# Пути к файлам и директориям
+PHOTO_DIR = os.getenv('PHOTO_DIR', r"/mnt/smb/OneDrive/Pictures/!Фотосессии/")
+DB_FILE = os.getenv('DB_FILE', os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "DB", "database.db")))
+REVIEW_DIR = os.getenv('REVIEW_DIR', "review")
+TELEGRAM_DB = os.getenv('TELEGRAM_DB', os.path.abspath(os.path.join(os.path.dirname(__file__), "../..", "telegram_bot", "published_photos.sqlite")))
 
 # Параметры базы данных
-TABLE_NAME = "photos_ok"
+TABLE_NAME = os.getenv('TABLE_NAME', "photos_ok")
 
 # Пороговые значения для классификации
-NSFW_THRESHOLD = 0.8
-CLIP_THRESHOLD = 0.8
-MIN_IMAGE_SIZE = 2500  # минимальный размер изображения (ширина или высота)
+NSFW_THRESHOLD = float(os.getenv('NSFW_THRESHOLD', "0.8"))
+CLIP_THRESHOLD = float(os.getenv('CLIP_THRESHOLD', "0.8"))
+MIN_IMAGE_SIZE = int(os.getenv('MIN_IMAGE_SIZE', "2500"))  # минимальный размер изображения (ширина или высота)
 
 # Статусы фотографий
 STATUS_REVIEW = "review"
